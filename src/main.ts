803d22e3ba9b75522e6c9c6ba7cb816112971ddd
Yuriy Ovdeyev <fido6080net@gmail.com>
2025-12-27 21:11:15 +0500

    Use shared SVG icon sprite

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import "jodit/es2015/jodit.min.css";
 import "highlight.js/styles/github.css";
 import "./styles/main.scss";
+import iconsSprite from "./assets/icons.svg?raw";
 import { mountApp } from "./vanilla/appShell";
+
+const ensureIconSprite = () => {
+  if (document.getElementById("app-icons")) return;
+  document.body.insertAdjacentHTML("afterbegin", iconsSprite);
+};
 
 const root = document.getElementById("root");
 if (root) {
+  ensureIconSprite();
   mountApp(root);
 }
